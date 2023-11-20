c8aefa09036cea9b6d613c9b14dbab9f0a235ee5
Dominik Stefancic <119432963+ImBoB99@users.noreply.github.com>
2023-11-20 17:48:28 +0100

    Added New Book button functionality

--- script.js
+++ script.js
@@ -24,37 +24,52 @@ function Book(title, author, pages) {
     this.pages = pages;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function addBookToLibrary(bookTitle, bookAuthor, bookPages) {
+        let book = new Book(bookTitle, bookAuthor, bookPages);
+        myLibrary.push(book);
+        updateBookList(book);
 }
 
-function updateBookList() {
+function updateBookList(book) {
     const booksGrid = document.querySelector(".books-grid");
-    myLibrary.forEach(function (book) {
 
+    const singleBook = document.createElement("div");
+    singleBook.classList.add("book");
 
-        const singleBook = document.createElement("div");
-        singleBook.classList.add("book");
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = book.title;
 
-        const bookTitle = document.createElement("p")
-        bookTitle.textContent = book.title;
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
 
-        const bookAuthor = document.createElement("p")
-        bookAuthor.textContent = book.author;
+    const bookPages = document.createElement("p");
+    bookPages.textContent = book.pages;
 
-        const bookPages = document.createElement("p")
-        bookPages.textContent = book.pages;
+    singleBook.appendChild(bookTitle);
+    singleBook.appendChild(bookAuthor);
+    singleBook.appendChild(bookPages);
 
-        singleBook.appendChild(bookTitle);
-        singleBook.appendChild(bookAuthor);
-        singleBook.appendChild(bookPages);
-
-        booksGrid.appendChild(singleBook);
-
-    })
-
+    booksGrid.appendChild(singleBook);
 }
 
-const addBookButton = document.querySelector(".add-book-btn")
+const addBookButton = document.querySelector(".add-book-btn");
+const cancelBookButton = document.getElementById("cancel");
+const confirmBookButton = document.querySelector(".confirm-button")
+const dialog = document.getElementById("bookDialog");
 
-updateBookList();
+const inputBookTitle = document.getElementById("bookTitle");
+const inputBookAuthor = document.getElementById("bookAuthor");
+const inputBookPages = document.getElementById("bookPages");
+
+addBookButton.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+cancelBookButton.addEventListener("click", () => {
+    dialog.close();
+})
+
+confirmBookButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog.close(addBookToLibrary(inputBookTitle.value, inputBookAuthor.value, inputBookPages.value));
+})
