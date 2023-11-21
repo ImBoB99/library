64e37c0c0ddefce7a5fe3ab987018456b7a54698
Dominik Stefancic <119432963+ImBoB99@users.noreply.github.com>
2023-11-21 10:58:15 +0100

    Initial project finished

--- script.js
+++ script.js
@@ -1,40 +1,27 @@
-const myLibrary = [
-    {
-        "title": "The Hobbit",
-        "author": "J.R.R. Tolkien",
-        "pages": 295,
-    },
+const myLibrary = [];
 
-    {
-        "title": "Some Guy",
-        "author": "Martin Luther",
-        "pages": 365,
-    },
-
-    {
-        "title": "Lord of the Rings",
-        "author": "J.R.R. Tolkien",
-        "pages": 482,
-    },
-];
-
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.read = read;
+    this.toggleRead = function() {
+        this.read = !this.read;
+    }
 }
 
-function addBookToLibrary(bookTitle, bookAuthor, bookPages) {
-        let book = new Book(bookTitle, bookAuthor, bookPages);
+function addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead) {
+        let book = new Book(bookTitle, bookAuthor, bookPages, bookRead);
         myLibrary.push(book);
-        updateBookList(book);
+        updateBookList(book, myLibrary.length - 1);
 }
 
-function updateBookList(book) {
+function updateBookList(book, index) {
     const booksGrid = document.querySelector(".books-grid");
 
     const singleBook = document.createElement("div");
     singleBook.classList.add("book");
+    singleBook.setAttribute("data-index", index);
 
     const bookTitle = document.createElement("p");
     bookTitle.textContent = book.title;
@@ -45,11 +32,45 @@ function updateBookList(book) {
     const bookPages = document.createElement("p");
     bookPages.textContent = book.pages;
 
+    // Button to toggle read status
+    const toggleReadButton = document.createElement("button");
+    toggleReadButton.innerText = book.read ? "Read" : "Not Read";
+    toggleReadButton.classList.add(book.read ? "read" : "unread");
+    toggleReadButton.addEventListener("click", () => {
+        book.toggleRead(); // Toggle the book's read status
+        renderBooks(); // Update the display after toggling the status
+    });
+    
+
+    const bookDelete = document.createElement("button");
+    bookDelete.classList.add("delete-button");
+    bookDelete.innerText = "Delete Book";
+    bookDelete.addEventListener("click", (e) => {
+        const index = e.target.parentElement.getAttribute("data-index");
+        removeBook(index);
+    })
+
     singleBook.appendChild(bookTitle);
     singleBook.appendChild(bookAuthor);
     singleBook.appendChild(bookPages);
+    singleBook.appendChild(toggleReadButton);
+    singleBook.appendChild(bookDelete);
 
     booksGrid.appendChild(singleBook);
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    renderBooks();
+}
+
+function renderBooks() {
+    const booksGrid = document.querySelector(".books-grid");
+    booksGrid.innerHTML = "";
+
+    myLibrary.forEach((book, index) => {
+        updateBookList(book, index);
+    });
 }
 
 const addBookButton = document.querySelector(".add-book-btn");
@@ -60,6 +81,7 @@ const dialog = document.getElementById("bookDialog");
 const inputBookTitle = document.getElementById("bookTitle");
 const inputBookAuthor = document.getElementById("bookAuthor");
 const inputBookPages = document.getElementById("bookPages");
+const inputBookRead = document.getElementById("bookRead");
 
 addBookButton.addEventListener("click", () => {
     dialog.showModal();
@@ -71,5 +93,9 @@ cancelBookButton.addEventListener("click", () => {
 
 confirmBookButton.addEventListener("click", (event) => {
     event.preventDefault();
-    dialog.close(addBookToLibrary(inputBookTitle.value, inputBookAuthor.value, inputBookPages.value));
+
+    const isBookRead = inputBookRead.checked;
+
+    dialog.close();
+    addBookToLibrary(inputBookTitle.value, inputBookAuthor.value, inputBookPages.value, isBookRead);
 })
